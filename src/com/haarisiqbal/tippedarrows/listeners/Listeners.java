84b556d59132dcc0704bceaa32e501f568252513
Haaris <58555509+HaarisIqbal@users.noreply.github.com>
2020-12-30 13:20:51 +0500

    Project v1.0
    First version of the project, currently only inducing a blindness effect.

---
+++ src/com/haarisiqbal/tippedarrows/listeners/Listeners.java
@@ -0,0 +1,37 @@
+package com.haarisiqbal.tippedarrows.listeners;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class Listeners implements Listener {
+  
+  @EventHandler
+  public void skelShot(EntityShootBowEvent shootEvent, ProjectileHitEvent hitEvent) {
+    
+    EntityType e = shootEvent.getEntityType();
+    
+    if (e == EntityType.SKELETON) {
+      arrHit(hitEvent);
+    }
+    
+  }
+  
+  @EventHandler
+  public void arrHit(ProjectileHitEvent hitEvent) {
+    
+    // 20 ticks = 1 Second
+    
+    PotionEffect p = new PotionEffect(PotionEffectType.BLINDNESS, 200, 1);
+    
+    LivingEntity l = (LivingEntity) hitEvent.getHitEntity();
+    l.addPotionEffect(p);
+    
+  }
+  
+}
