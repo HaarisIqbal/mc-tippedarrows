23ee1758a9856d71708433ae51cbce9f7e31a7c4
Haaris Iqbal <haaris.n.iqbal@gmail.com>
2020-12-30 23:37:21 +0500

    Complete Implementation
    Implemented switch statement to apply random potion effect to player.

--- src/com/haarisiqbal/tippedarrows/listeners/Listeners.java
+++ src/com/haarisiqbal/tippedarrows/listeners/Listeners.java
@@ -1,5 +1,6 @@
 package com.haarisiqbal.tippedarrows.listeners;
 
+import java.util.Random;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -11,6 +12,8 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class Listeners implements Listener {
+  
+  Random rand = new Random();
   
   @EventHandler
   public void skelShot(EntityShootBowEvent shootEvent, ProjectileHitEvent hitEvent) {
@@ -26,13 +29,32 @@ public class Listeners implements Listener {
   @EventHandler
   public void arrHit(ProjectileHitEvent hitEvent) {
     
-    // 20 ticks = 1 Second
+    int ticks = 200; // 20 ticks = 1 Second.
+    PotionEffect p = new PotionEffect(PotionEffectType.HEAL, ticks, 1);
     
-    PotionEffect p = new PotionEffect(PotionEffectType.BLINDNESS, 200, 1);
+    switch(rand.nextInt(5)) {
+      case 0:
+        p = new PotionEffect(PotionEffectType.HUNGER, ticks, 1);
+        break;
+      case 1:
+        p = new PotionEffect(PotionEffectType.BLINDNESS, ticks, 1);
+        break;
+      case 2:
+        p = new PotionEffect(PotionEffectType.POISON, ticks, 1);
+        break;
+      case 3:
+        p = new PotionEffect(PotionEffectType.LEVITATION, ticks/2, 1);
+        break;
+      case 4:
+        p = new PotionEffect(PotionEffectType.CONFUSION, ticks, 1);
+        break;
+      default:
+        break;
+    }
     
     Entity e = hitEvent.getHitEntity();
     
-    if (e != null) {
+    if (e != null) { // bug fix, ensuring living entity.
       LivingEntity l = (LivingEntity) hitEvent.getHitEntity();
       l.addPotionEffect(p);
     }
