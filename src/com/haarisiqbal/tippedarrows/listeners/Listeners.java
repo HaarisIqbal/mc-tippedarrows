7218b587d0a292c35d16d2d99c5b802980409a12
Haaris Iqbal <haaris.n.iqbal@gmail.com>
2020-12-31 17:22:34 +0500

    Other Entity Shot Fix
    Fixed flaw in which all arrow collisions caused the potion effect. Restricted to skeletons by using global boolean variable.

--- src/com/haarisiqbal/tippedarrows/listeners/Listeners.java
+++ src/com/haarisiqbal/tippedarrows/listeners/Listeners.java
@@ -14,20 +14,25 @@ import org.bukkit.potion.PotionEffectType;
 public class Listeners implements Listener {
   
   Random rand = new Random();
+  boolean otherMob = true; // to run code only when it is skeleton and not other entity.
   
   @EventHandler
-  public void skelShot(EntityShootBowEvent shootEvent, ProjectileHitEvent hitEvent) {
+  public void skelShot(EntityShootBowEvent shootEvent) {
     
     EntityType e = shootEvent.getEntityType();
     
     if (e == EntityType.SKELETON) {
-      arrHit(hitEvent);
+      otherMob = false;
     }
     
   }
   
   @EventHandler
   public void arrHit(ProjectileHitEvent hitEvent) {
+    
+    if (otherMob) {
+      return;
+    }
     
     int ticks = 200; // 20 ticks = 1 Second.
     PotionEffect p = new PotionEffect(PotionEffectType.HEAL, ticks, 1);
@@ -58,6 +63,8 @@ public class Listeners implements Listener {
       LivingEntity l = (LivingEntity) hitEvent.getHitEntity();
       l.addPotionEffect(p);
     }
+    
+    otherMob = true;
     
   }
   
